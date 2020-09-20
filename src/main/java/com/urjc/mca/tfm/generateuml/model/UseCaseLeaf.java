cf20920e06fe70ca8576f2af8dc41b56ef8538d9
pablo.calvo <pablo.calvo@babel.es>
2020-09-20 15:20:54 +0200

    add useCase with Composite pattern

---
+++ src/main/java/com/urjc/mca/tfm/generateuml/model/UseCaseLeaf.java
@@ -0,0 +1,31 @@
+package com.urjc.mca.tfm.generateuml.model;
+
+import java.util.Objects;
+
+public class UseCaseLeaf implements UseCase{
+
+    private String name;
+
+    public UseCaseLeaf(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UseCase useCase = (UseCase) o;
+        return Objects.equals(name, useCase.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+}
