40ee21ad747f7d8a9524fd3c8fe32b4846472f47
pablo.calvo <pablo.calvo@babel.es>
2020-09-20 15:20:22 +0200

    add Actor

---
+++ src/main/java/com/urjc/mca/tfm/generateuml/model/Actor.java
@@ -0,0 +1,31 @@
+package com.urjc.mca.tfm.generateuml.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Actor {
+
+    public final String name;
+    private Set<UseCase> useCases = new HashSet<>();
+
+    public Actor(String name){
+        this.name = name;
+    }
+
+    public void addUseCase(UseCase useCase){
+        if(!exitsUseCase(useCase))
+            useCases.add(useCase);
+    }
+
+    private boolean exitsUseCase(UseCase useCase){
+        return this.useCases.stream().anyMatch(us -> us.getName().equals(useCase.getName()));
+    }
+    public Set<UseCase> getUseCases(){
+        return this.useCases;
+    }
+
+//    public void addUsedCaseComposie(UseCase useCase){
+//
+//    }
+
+}
