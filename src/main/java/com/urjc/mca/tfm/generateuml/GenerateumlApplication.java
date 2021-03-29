7b316901d777cfe51d5d07893f6d5efa9d5fa5c5
pablo.calvo <pablo.calvo@babel.es>
2021-03-29 10:16:52 +0200

    refactor class

--- src/main/java/com/urjc/mca/tfm/generateuml/GenerateumlApplication.java
+++ src/main/java/com/urjc/mca/tfm/generateuml/GenerateumlApplication.java
@@ -3,20 +3,30 @@ package com.urjc.mca.tfm.generateuml;
 import org.eclipse.core.commands.ExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.script.ScriptException;
 import java.io.IOException;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class GenerateumlApplication implements CommandLineRunner {
 
     private static Logger log = LoggerFactory
             .getLogger(GenerateumlApplication.class);
 
-    public static void main(String[] args) {
+    @Autowired
+    JavaAnalyzerEclipseAST javaAnalyzerEclipseAST;
+
+
+    @Autowired
+    GenerateUml generateUml;
+
+    public static void main(String[] args) throws NoSuchMethodException, ScriptException, IOException {
         log.info("STARTING THE APPLICATION");
         SpringApplication.run(GenerateumlApplication.class, args);
         log.info("APPLICATION FINISHED");
@@ -25,7 +35,8 @@ public class GenerateumlApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws NoSuchMethodException, ScriptException, IOException, ExecutionException {
 
-        GenerateUml generateUml = new GenerateUml();
-        generateUml.run("https://github.com/pjcalvo84/damas.git", "damas");
+        if(args.length > 0) {
+            generateUml.run(args[0], args[1]);
+        }
     }
 }
