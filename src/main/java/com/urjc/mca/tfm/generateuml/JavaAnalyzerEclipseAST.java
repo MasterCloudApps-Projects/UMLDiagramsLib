29b44d58b3559af96cf0a7400f9db9bc9f4ab64e
pablo.calvo <pablo.calvo@babel.es>
2021-04-09 00:28:54 +0200

    solved sonar bug issue

--- src/main/java/com/urjc/mca/tfm/generateuml/JavaAnalyzerEclipseAST.java
+++ src/main/java/com/urjc/mca/tfm/generateuml/JavaAnalyzerEclipseAST.java
@@ -46,7 +46,7 @@ public class JavaAnalyzerEclipseAST {
 
         hit = Optional.ofNullable(Path.of(path));
         try (Stream<Path> entries
-                     = Files.walk(Paths.get(hit.stream().findFirst().get().toString()).toAbsolutePath())) {
+                     = Files.walk(Paths.get(hit.stream().findAny().orElse(Paths.get("")).toString()).toAbsolutePath())) {
             entries.forEach(ruta -> {
                 if (Files.isRegularFile(ruta)) {
                     try {
